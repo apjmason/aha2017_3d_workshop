a4cbfb6e9c31bee51eeffaa2dc3abe5642de8e46
apjmason <austin.pj.mason@gmail.com>
2017-01-03 17:04:45 -0600

    Added script for mouse control and baked lighting

---
+++ MuseumExample/Assets/scripts/MouseScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour {
+
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void Update()
+	{
+		var fwd = transform.TransformDirection (Vector3.forward);
+		RaycastHit hit;
+		Debug.DrawRay(transform.position, fwd * 2, Color.green);
+
+		if (Physics.Raycast (transform.position, fwd, out hit, 10)) {  // Perform Raycast
+			
+			if (hit.collider.tag == "Artifact") {  // check tag
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+
+
+		}
+	}
+
+
+	//	// press Escape to toggle the cursor on and off
+//	void Start()
+//	{
+//		Cursor.lockState = CursorLockMode.Locked;
+//		Cursor.visible = false;
+//	}
+//
+//	void Update()
+//	{
+//		Cursor.lockState = CursorLockMode.Locked;
+//		Cursor.visible = false;
+//	}
+
+}
